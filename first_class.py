f47a8d961ffbe82b7ecd6b8a54d6fc7e7a633971
Akib Samir <49079848+AkibSamir@users.noreply.github.com>
2021-09-23 23:34:50 +0600

    first class

--- first_class.py
+++ first_class.py
@@ -1,1 +1,57 @@
 print("Hello World")
+
+# Conditional Statement
+# if else statement
+"""
+when is_present = True then print if statement
+when is_present = False then print else statement
+"""
+is_present = True
+if  is_present:
+    print("Good Boy")
+else:
+    print("Bad Boy")
+
+# if elif else statement
+cgpa = float(input("Enter your CGPA(0-4): "))
+"""
+Remember python always get input as a string
+if we use it as any other data type we need to convert it
+and we convert our input data as floating value
+It also known as type casting 
+"""
+
+if cgpa == 4.0:
+    print("A+")
+elif cgpa >= 3.75 and cgpa < 4.0:
+    print("A")
+elif cgpa >= 3.50 and cgpa < 3.75:
+    print("A-")
+elif cgpa >= 3.25 and cgpa < 3.50:
+    print("B+")
+elif cgpa >= 3.0 and cgpa < 3.25:
+    print("B")
+elif cgpa >= 2.75 and cgpa < 3.0:
+    print("B-")
+elif cgpa >= 2.50 and cgpa < 2.75:
+    print("C+")
+elif cgpa >= 2.25 and cgpa < 2.50:
+    print("C")
+elif cgpa >= 2.0 and cgpa < 2.25:
+    print("D")
+else:
+    print("F")
+
+# Nested if
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Your age is okay. You need to bring your NID with you.")
+    nid = input("Do you have NID? Y/N: ")
+    if nid.upper() == "Y":
+        print("Go and give a vote.")
+    else:
+        print("Sorry! Go back home and bring your NID.")
+else:
+    print("You are a teeneger. You are not elligible to giving the vote")
+
+
